f0322013bd44b76b9334f68faa0a279fc1904b66
Samuel Chamalé <samuel.chamale23@gmail.com>
2023-11-03 16:54:22 -0600

    assets: final test

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from src.utils.tools import readYaml
 from src.turing import Turing
 
+path = './assets/LAB9E2.yaml'
 path = './assets/LAB9E3.yaml'
-# path = './assets/LAB9E2.yaml'
+path = './assets/LAB10E1.yaml'
+
 crudeConfig = readYaml(path)
 turing = Turing(crudeConfig)
 
